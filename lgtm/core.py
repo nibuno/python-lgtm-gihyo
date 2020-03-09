ba1d1c2f291f0fd50edad3ad99758172aadb7dfc
Yumihiki <nibu10.majik@gmail.com>
2020-03-09 17:42:01 +0900

    P337の箇所まで調べた内容記述

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,12 +1,18 @@
+# main.pyから呼び出されている
+
 import click
+# コマンドラインツール作成ライブラリ
 from lgtm.drawer import save_with_message
 from lgtm.image_source import get_image
 
 
 @click.command()
+# コマンドとして実行したい関数につける
 @click.option('--message', '-m', default='LGTM',
               show_default=True, help='画像に乗せる文字列')
+# オプションで渡すものを指定
 @click.argument('keyword')
+# 位置引数で渡すものを指定
 def cli(keyword, message):
     """LGTM画像生成ツール"""
     lgtm(keyword, message)
--- lgtm/image_source.py
+++ lgtm/image_source.py
@@ -1,4 +1,6 @@
 import requests
+# HTTPクライアントライブラリ
+# HTTPリソースを簡単に利用するためのパッケージ
 from io import BytesIO
 from pathlib import Path
 
@@ -8,9 +10,23 @@ class LocalImage:
 
     def __init__(self, path):
         self._path = path
+        # __init__(): インスタンスの初期化を行う特殊メソッド
+        # 第一引数にインスタンス自身が渡ってくる
+        # ここでインスタンスに属性を追加すると、このクラスのすべてのインスタンスがその属性を持つことになる
+
+        # P128-129
 
     def get_image(self):
         return open(self._path, 'rb')
+        # get_image()を呼び出すと画像のファイルオブジェクトを返す
+        # 今回画像の取得で利用するすべてのクラスでこのメソッドを呼び出すと
+        # 画像のファイルオブジェクトを返すようにする
+
+        # open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+        # fileを開き、対応するファイルオブジェクトを返す
+        # r: 読み込み用に開く（デフォルト）
+        # b: バイナリモード
+        # https://docs.python.org/ja/3/library/functions.html#open
 
 
 class RemoteImage:
@@ -18,6 +34,7 @@ class RemoteImage:
 
     def __init__(self, path):
         self._url = path
+        # LocalImageクラスの__init__()と同じ
 
     def get_image(self):
         data = requests.get(self._url)
@@ -26,21 +43,41 @@ class RemoteImage:
 
 
 class _LoremFlickr(RemoteImage):
+    # クラス定義名の後に(基底クラス名)をつけると基底クラスの性質を継承したサブクラスを定義できる
+    # クラスの継承という
+    # RemoteImageのメソッドをそのまま利用しつつ新しいメソッドや変数を追加、上書きできる。
+    # _の作法ってなに？
     """キーワード検索で画像を取得する"""
 
     LOREM_FLICKER_URL = 'https://loremflickr.com'
     WIDTH = 800
     HEIGHT = 600
+    # ex: https://loremflickr.com/WIDTH/HEIGHT/KEYWORD
+    # にアクセスすると、指定サイズ・キーワードに沿ったランダムな画像を返してくれる
 
     def __init__(self, keyword):
         super().__init__(self._build_url(keyword))
+        # super(): 基底クラスのメソッドを呼び出す
+        # これで返されるオブジェクトはプロキシオブジェクトでありクラスオブジェクトそのものではない
+        # 今回の場合、RemoteImageの__init__()メソッドを呼び出している
+
+        # P140
 
     def _build_url(self, keyword):
         return (f'{self.LOREM_FLICKER_URL}/'
-                f'{self.WIDTH}/{self.HEIGHT}/{keyword}')
+                f'{self.WIDTH}/{self.HEIGHT}/{keyword}'
+                # fで始まっているのはf-string: 式を埋め込める文字列リテラル
+                # 文字列中に{}でくくった変数や式を記述すると、実行時に{}が評価され、その結果に置き換えられる
+
+                # P76
+                )
 
 
 KeywordImage = _LoremFlickr
+# 内部用にLomenFlickrを利用していることがわかるように
+# _LomenFlickr クラスを定義してKeywordImageという別名で参照できるように
+# メリットとして、LomenFlickrから別のサービスを利用する際に
+# クラスを差し替えるだけで済むようになる点が挙げられる
 
 
 # コンストラクタとして利用するため
@@ -48,6 +85,8 @@ KeywordImage = _LoremFlickr
 def ImageSource(keyword):
     """最適なイメージソースクラスを返す"""
     if keyword.startswith(('http://', 'https://')):
+        # http://またはhttps://で始まるならTrue
+        # https://docs.python.org/ja/3/library/stdtypes.html?highlight=startswith#str.startswith
         return RemoteImage(keyword)
     elif Path(keyword).exists():
         return LocalImage(keyword)
@@ -58,3 +97,6 @@ def ImageSource(keyword):
 def get_image(keyword):
     """画像のファイルオブジェクトを返す"""
     return ImageSource(keyword).get_image()
+    # get_image()でImageSource()メソッドを利用することにより
+    # クラスのように見せる
+
